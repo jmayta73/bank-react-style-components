f69a550166f50606ef9d597f4c30c1937c68d886
jmayta73 <ingmaytae@hotmail.com>
2023-05-28 15:29:04 -0500

    fin segunda clase style react

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from "react";
 
 import Header from "./Components/Header";
 import Container from "./Components/Container";
+import GloblaStyle from "./GlobalStyle";
 
 function App() {
   return (
     <>
+      <GloblaStyle />
       <Header />
       <Container />
     </>
--- src/Components/Title/index.jsx
+++ src/Components/Title/index.jsx
@@ -1,4 +1,9 @@
-import React from "react";
+
+
+// el import React from "react" se quita porque en el proyecto react no se esta usando
+// cuando se usa style-components como en 
+// const Titlr = styled.h1 despeues de esto se pone las tilde al reves para poner los comandos como ==> `color: grey`
+//tamnien se instala la extension STYLED-COMPONENTS-REACT PARA QUE NOS AUTOCOMPPLETE
 import styled from "styled-components";
 const Title = styled.h1 `
   color: grey;
